6a58ea0d7952b09c7850736a228a09c8e47188f4
CJ <cj@bluebird.dev>
2020-04-23 07:57:26 +0200

    add various analytics

---
+++ src/analytics/clicky.js
@@ -0,0 +1,3 @@
+(function(){
+    var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101249775);
+})();
--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import DefaultLayout from '~/layouts/Default.vue'
 require('typeface-courier-prime')
 require('typeface-montserrat')
 require('normalize.css')
+require('@/analytics/leadfeeder.js')
+require('@/analytics/clicky.js')
+require('@/analytics/segment.js')
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
@@ -19,5 +22,10 @@ export default function (Vue, { router, head, isClient }) {
   head.meta.push({
     name: 'Description',
     content: 'Bluebird Development: Creating future tech with style and simplicity.'
+  });
+
+  head.script.push({
+    src: '//static.getclicky.com/js',
+    body: true
   });
 }
