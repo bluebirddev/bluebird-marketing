c4c16e2962be9b6eb056ec7194eea835bd1e66df
CJ <cj@bluebird.dev>
2021-06-06 18:20:20 +0200

    new fame.shopping logo

--- src/components/Clients.vue
+++ src/components/Clients.vue
@@ -9,6 +9,10 @@
         </svg>
       </h2>
       <div class="clients">
+        <a href="https://fame.shopping" target="_blank">
+          <span>FameShopping</span>
+          <g-image src="~/assets/clients/fame.svg" alt="fame" width="96" />
+        </a>
         <a href="https://22seven.com" target="_blank">
           <span>22seven</span>
           <g-image src="~/assets/clients/22seven.svg" alt="22seven" width="96" />
@@ -21,29 +25,9 @@
           <span>bank.tech</span>
           <g-image src="~/assets/clients/banktech.svg" alt="banktech" width="96" />
         </a>
-        <a href="https://coindirect.com" target="_blank">
-          <span>coindirect.com</span>
-          <g-image src="~/assets/clients/coindirect.svg" alt="coindirect" width="96" />
-        </a>
         <a href="https://www.thedelta.io" target="_blank">
           <span>thedelta</span>
           <g-image src="~/assets/clients/delta.png" alt="delta" width="180" style="width:90px;" />
-        </a>
-        <a href="https://www.duepay.co.za/" target="_blank">
-          <span>duepay</span>
-          <g-image src="~/assets/clients/duepay.png" alt="duepay" width="96" />
-        </a>
-        <a href="https://www.evuna.co.za/" target="_blank">
-          <span>evuna</span>
-          <g-image src="~/assets/clients/evuna.svg" alt="evuna" width="96" />
-        </a>
-        <a href="https://layup.co.za" target="_blank">
-          <span>layup</span>
-          <g-image src="~/assets/clients/layup.png" alt="layup" width="172" style="width:86px;" />
-        </a>
-        <a href="https://www.pabtglobal.com" target="_blank">
-          <span>pump-and-abrasion</span>
-          <g-image src="~/assets/clients/pump-and-abrasion.png" alt="pump-and-abrasion" width="200" style="width:100px;" />
         </a>
         <a href="https://www.raubex.co.za" target="_blank">
           <span>raubex</span>
@@ -51,7 +35,27 @@
         </a>
         <a href="https://revix.com" target="_blank">
           <span>revix</span>
-          <g-image src="~/assets/clients/revix.png" alt="revix" width="172" style="width:86px;" />
+          <g-image src="~/assets/clients/revix.png" alt="revix" width="172" style="width:76px;" />
+        </a>
+        <a href="https://layup.co.za" target="_blank">
+          <span>layup</span>
+          <g-image src="~/assets/clients/layup.png" alt="layup" width="172" style="width:80px;" />
+        </a>
+        <a href="https://www.evuna.co.za/" target="_blank">
+          <span>evuna</span>
+          <g-image src="~/assets/clients/evuna.svg" alt="evuna" width="96" />
+        </a>
+        <a href="https://www.duepay.co.za/" target="_blank">
+          <span>duepay</span>
+          <g-image src="~/assets/clients/duepay.png" alt="duepay" width="96" />
+        </a>
+        <a href="https://coindirect.com" target="_blank">
+          <span>coindirect.com</span>
+          <g-image src="~/assets/clients/coindirect.svg" alt="coindirect" width="96" />
+        </a>
+        <a href="https://www.pabtglobal.com" target="_blank">
+          <span>pump-and-abrasion</span>
+          <g-image src="~/assets/clients/pump-and-abrasion.png" alt="pump-and-abrasion" width="200" style="width:100px;" />
         </a>
         <a href="https://thorchain.org/" target="_blank">
           <span>THORChain</span>
@@ -87,8 +91,7 @@ export default {
 }
 .title {
   color: #fff;
-  width: 350px;
-  margin-right: 210px;
+  width: 300px;
 }
 .underline {
   margin-top: 10px;
@@ -101,12 +104,13 @@ polyline {
 }
 .clients {
   display: flex;
-  flex: 1;
   justify-content: flex-end;
   flex-wrap: wrap;
   align-items: center;
+  width: 630px;
+  margin-left: auto;
   a {
-    width: 130px;
+    margin: 10px;
     text-align: center;
     > span {
       display: none;
@@ -122,7 +126,7 @@ polyline {
   }
 }
 
-// mobile
+// non-desktop
 @media only screen and (max-width: 1100px) {
   .content {
     flex-direction: column;
@@ -134,9 +138,21 @@ polyline {
   .clients {
     justify-content: space-around;
     margin-top: 30px;
+    width: 100%;    
+    a {
+      margin: 5px 0;
+    }
   }
   .wrapper {
     padding: 60px 0;
+  }
+}
+
+
+// mobile
+@media only screen and (max-width: 400px) {
+  .clients a {
+    width: 50%;
   }
 }
 </style>
